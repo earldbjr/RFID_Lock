159a7ffa4d00208937ada2b0b9463b60a53296d6
Gizmo <earldbjr@gmail.com>
2014-08-29 18:37:45 -0400

    Seems to be powercycling

--- RFID_Lock.ino
+++ RFID_Lock.ino
@@ -7,9 +7,10 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);        // Create MFRC522 instance.
 
 int redLed1 = A4;//Door locked, Analog pin0.
 int greenLed1 = A5;//Door unlocked, Analog pin1.
-int reed = 8;
+int reed = 2;
 int lockPin1 = 7;
 int lockPin2 = 6;
+int safeToLock = 0;
 String card1 = "2454512237";
 String card2 = "2454512237";
 String card3 = "2454512237";
@@ -17,8 +18,13 @@ String card4 = "2454512237";
 String card5 = "2454512237";
 String card6 = "2454512237";
 /*Blacklisted numbers:
-2445113213 Blank white card - Lost
-*/
+ 2445113213 Blank white card - Lost
+ */
+void checkReed();
+void offLock();
+void unlockDoor();
+void lockDoor();
+void reedRising();
 void setup() {
   //Serial.begin(9600);        // Initialize serial communications with the PC
   SPI.begin();                // Initialize SPI bus
@@ -28,6 +34,55 @@ void setup() {
   pinMode(redLed1, OUTPUT);
   pinMode(greenLed1, OUTPUT);
   pinMode(reed, INPUT);
+  attachInterrupt(0,reedRising, RISING); //  0=pin2, 1=pin3
+}
+
+void loop() {
+  checkReed();
+  static int firstRun = 0;
+  if(firstRun == 0){ //On power on. If door is open, unlock. If door is closed, lock.
+    firstRun = 1;
+    lockDoor();
+
+  }
+  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+  MFRC522::MIFARE_Key key;
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    checkReed();
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    checkReed();
+    return;
+  }
+  // Now a card is selected. The UID and SAK is in mfrc522.uid.
+
+  // Dump UID
+  //Serial.print("Card UID:");
+  String idRead;
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    //Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    //Serial.print(mfrc522.uid.uidByte[i]);//, HEX);
+    //idRead.concat(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    idRead.concat(mfrc522.uid.uidByte[i]);
+  } 
+  //Serial.print(idRead);
+  //Serial.println();
+  //if (idRead == officeCard) {
+  //    digitalWrite(redLed1, HIGH);
+  //    digitalWrite(greenLed1, HIGH);
+  //}
+
+  if (idRead == card1 || idRead == card2 || idRead == card3 || idRead == card4 || idRead == card5 || idRead == card6){
+    unlockDoor();
+
+  }
 }
 
 void offLock(){
@@ -54,99 +109,25 @@ void lockDoor(){
   offLock();
 }
 
+void reedRising(){
+  7safeToLock = 1;
+}
 
-
-void loop() {
-  static int firstRun = 0;
-  if(firstRun == 0){ //On power on. If door is open, unlock. If door is closed, lock.
-    firstRun = 1;
-    lockDoor();
-    digitalWrite(redLed1, HIGH);
-    digitalWrite(greenLed1, HIGH);
-  } 
-  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
-  MFRC522::MIFARE_Key key;
-  for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
-  }
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
-
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
-  // Now a card is selected. The UID and SAK is in mfrc522.uid.
-
-  // Dump UID
-  //Serial.print("Card UID:");
-  String idRead;
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    //Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    //Serial.print(mfrc522.uid.uidByte[i]);//, HEX);
-    //idRead.concat(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    idRead.concat(mfrc522.uid.uidByte[i]);
-  } 
-  //Serial.print(idRead);
-  //Serial.println();
-  //if (idRead == officeCard) {
-  //    digitalWrite(redLed1, HIGH);
-  //    digitalWrite(greenLed1, HIGH);
-  //}
-
-  if (idRead == card1 || idRead == card2 || idRead == card3 || idRead == card4 || idRead == card5 || idRead == card6){
-    unlockDoor();
-    if(digitalRead(reed) == 1){
-      while(digitalRead(reed) == 1){
-      }
-antibounce:
-      while(digitalRead(reed) == 0){
-      }
-      if(digitalRead(reed) == 1){
-        delay(100);
-        if(digitalRead(reed) == 1){
-          delay(3000);
-          lockDoor();
-        }
-        else{ 
-          goto antibounce; 
-        }
+void checkReed(){
+  if(safeToLock == 1){
+    int time = millis();
+    int safe = 1;
+    while(millis()-time <= 2000){
+      if(digitalRead(reed) == 0){
+        safe = 0;
       }
     }
-    else {
-      antibounce2:
-      while(digitalRead(reed) == 0){
-      }
-      if(digitalRead(reed) == 1){
-        delay(100);
-        if(digitalRead(reed) == 1){
-          delay(3000);
-          lockDoor();  
-        } else{ goto antibounce2;}
-      }
+    if(safe == 1){
+      safeToLock = 0;
+      lockDoor();
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
